ff628b3ce439e994980739e13264d5632212133b
hvsr1997 <rathoreharshvardhansingh1997@gmail.com>
2025-02-23 12:46:51 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Greet from "./components/greet";
+
+function App() {
+  return (
+    <section>
+      <Greet></Greet>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/greet.jsx
@@ -0,0 +1,5 @@
+const Greet = () => {
+  return <h1>Harsh</h1>;
+};
+
+export default Greet;
